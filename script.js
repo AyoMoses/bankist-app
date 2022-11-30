34d7faf7b9ca5fa4331a62f92541e7e1d834686e
Ayo Moses <ayo.odukoya@speakcreative.com>
2022-11-30 12:43:50 +0100

    array methods

--- script.js
+++ script.js
@@ -74,3 +74,57 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+//? ARRAY METHODS - KNOWING THAT ARRAYS ARE ALSO OBJECTS SINCE THEY TAKE METHODS MEANT FOR OBJECTS
+
+//? THE ARRAY SLICE METHOD 
+// let arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+// slice returns the index of an array indidcating starting or ending without mutating the original
+// it returns the copy of the array to slice
+// console.log(arr.slice(2));
+//using a negative interger means it will start from the end of the array. -2 means last two, -1 means the last and so on
+// console.log(arr.slice(-3));
+// the above returns ['d', 'e', 'f']
+// console.log(arr.slice(1, -2));
+// the above returns ['b', 'c', 'd']
+
+
+//? NOTE: we can use the slice() to create a shallow copy of an array. This is done without putting a value in the slice 
+// console.log(arr.slice());
+// we can also use the spread operator to create a shallow copy of an array 
+// console.log([...arr]);
+
+// the best time to use slice is when you need to chain multiple methods together
+
+//? SPLICE METHOD 
+// the splice method works the same way as the slice method and the only difference is that it changes the original array by deleting the array called in the parameter
+//? ONCE YOU USE SPLICE ON AN ARRAY, IT DELETES THEM FROM THE ORIGINAL ARRAY LEAVING THE ONES NOT CALLED IN SPLICE
+// console.log('i am the splice',arr.splice(2));
+// console.log('i am the remaining after splice',arr);
+
+//? the most common use case is to delete the last index of an array.
+// console.log('i am the deleted array', arr.splice(-1));
+// console.log('i am the remaining after splice', arr);
+// console.log('deleting more than one', arr.splice(1, 2));
+// console.log('after more than once is deleted', arr);
+// the above returns ['a', 'd', 'e', 'f']
+
+
+//? REVERSE() METHOD
+// it reverses the arrangement of an array
+// the reverse method actually mutates the original array
+let arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+const arr2 = ['l', 'k', 'j', 'i', 'h', 'g'];
+console.log(arr2.reverse());
+
+//? CONCAT METHOD 
+// concat method is used to add two arrays together. It does not mutate the original array
+let letters = arr.concat(arr2);
+console.log(letters);
+//? we can also use the spread operator to concat arrays together
+console.log([...arr, ...arr2]);
+
+
+//? JOIN METHOD 
+// the join method converts an array to a string by putting a seperator we intend to use 
+console.log(letters.join(' + '));
