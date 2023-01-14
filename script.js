a9bed47a6bedc7371c72129e3c9fdcd13c121b7f
ayomoses <odukoyaayodeji@gmail.com>
2023-01-14 21:46:54 +0100

    code cleanup

--- script.js
+++ script.js
@@ -475,7 +475,7 @@ btnSort.addEventListener('click', function (e) {
 
 
 // LECTURES
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
